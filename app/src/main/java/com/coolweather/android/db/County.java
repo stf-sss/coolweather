17789f50091d5e265dff3129590c4e323fddb555
stf <279418007@qq.com>
2020-06-21 16:19:02 +0800

    First commit

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,40 @@
+package com.coolweather.android.db;
+
+import android.provider.ContactsContract;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 27941 on 2020/6/19.
+ */
+
+public class County  extends DataSupport{
+    private int id;
+    private String countyName;
+    private  String weatherId;
+    private int cityId;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getCountyName(){
+        return countyName;
+    }
+    public void setCountyName(String countyName){
+        this.countyName=countyName;
+    }
+    public String getWeatherId(){
+        return weatherId;
+    }
+    public  void setWeatherId(String weatherId){
+        this.weatherId=weatherId;
+    }
+    public int getCityId(){
+        return cityId;
+    }
+    public void setCityId(int cityId){
+        this.cityId=cityId;
+    }
+}
