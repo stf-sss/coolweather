260a3c9d63869853b0eee653c89bdb3ec7b9c6fb
stf <279418007@qq.com>
2020-06-25 15:57:39 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by 27941 on 2020/6/23.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -1,5 +1,12 @@
 package com.coolweather.android.util;
 
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -13,4 +20,6 @@ public class HttpUtil {
         Request request =new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
     }
+
+
 }
